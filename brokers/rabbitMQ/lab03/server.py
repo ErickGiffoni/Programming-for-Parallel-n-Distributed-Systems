c2d75b50877a674b7be4290b88236e8c86861b88
ErickGiffoni <giffoni.erick@gmail.com>
2021-09-04 19:56:08 -0300

    ja envia e recebe mensagens dos workers - erro: na segunda vez que o publish faz um pedido, o server nao recebe alguma das mensagens do worker

--- brokers/rabbitMQ/lab03/server.py
+++ brokers/rabbitMQ/lab03/server.py
@@ -1,8 +1,14 @@
-import pika;
+from typing import List
+import pika, uuid;
 
 connection = pika.BlockingConnection(
    pika.ConnectionParameters(host="localhost")
 );
+
+worker_connection = pika.BlockingConnection(
+   pika.ConnectionParameters(host="localhost")
+);
+worker_channel = worker_connection.channel();
 
 channel = connection.channel();
 
@@ -27,6 +33,73 @@ channel.queue_bind(
 
 def find_bs_n_callback(ch, method, props, body):
    print("[x] Recebi um pedido...");
+
+   # divifir o trabalho p/ os workers
+   p_result: List = [];
+   correlation_id: List = [];
+
+   def find_partial_result():
+      find_p_bs_n_queue_name = "find_p_bs_n";   # find partial biggest and smallest number
+      worker_channel.queue_declare(
+         durable=True,
+         queue=find_p_bs_n_queue_name
+      );
+
+      message = "sending a hello world";
+
+      callback_queue_name = "found_p_bs_n";
+      found_p_bs_n_queue = worker_channel.queue_declare(
+         queue=callback_queue_name, 
+         durable=True, 
+         exclusive=True
+      ).method.queue;
+
+      message_count = 3;
+      i=0;
+      # correlation_id: List = [];
+      while i < message_count:
+         print("sending %r of %r" % (i+1, message_count));
+         correlation_id.append(str(uuid.uuid4())); # problema na hora de verificar a resposta q chega
+         worker_channel.basic_publish(
+            body=message,
+            routing_key=find_p_bs_n_queue_name,
+            properties=pika.BasicProperties(
+               correlation_id=correlation_id[i],
+               reply_to=found_p_bs_n_queue,
+               delivery_mode=2,
+               content_type=None
+            ),
+            exchange=""
+         );
+         i+=1;
+
+      def partial_response_callback(ch, method, properties, body):
+         #global p_result;
+         #if properties.correlation_id in correlation_id:
+         p_result.append(str(body));
+         # print("p_result: %r" % p_result);
+         # print("-> corr_id %r" % correlation_id);
+         ch.basic_ack(delivery_tag=method.delivery_tag);
+         #else:
+            # print("X. different correlation id");
+            # print("corr_id: %r" % correlation_id);
+            # print("props corr_id %r" % properties.correlation_id);
+         return;
+
+      worker_channel.basic_qos(prefetch_count=1);
+      worker_channel.basic_consume(
+         queue=callback_queue_name,
+         on_message_callback=partial_response_callback
+      );
+
+      print("[..] esperando respostas dos workers");
+      while len(p_result) < message_count:
+         worker_connection.process_data_events();
+      #correlation_id: List = [None];
+
+   find_partial_result();
+   print("-> mensagens chegaram: %r" % p_result);
+   # mandar o resultado final
 
    result = "OK!!!";
    ch.basic_publish(
